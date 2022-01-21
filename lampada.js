9c9d01c3f7f952bcc350775aed3e61220d3cd3dc
Isaac Yuri <isaacyuri22@gmail.com>
2022-01-21 09:50:58 -0300

    agora tem apenas um botao

--- lampada.js
+++ lampada.js
@@ -2,17 +2,26 @@ const lampadaLigada = './img/lampada acesa.jpg';
 const lampadaDesligada = './img/lampada apagada.jpg';
 const lampadaQuebrada = './img/lampada quebrada.jpg';
 const estadoDaLampada = document.querySelector('#estado-da-lampada');
-const botaoLigar = document.querySelector('#ligar');
-const botaoDesligar = document.querySelector('#desligar');
+const botaoLigarDesligar = document.querySelector('#ligar-desligar');
 
 estadoDaLampada.addEventListener('dblclick', quebrarLampada);
 estadoDaLampada.addEventListener('mouseout', desligarLampada);
-botaoLigar.addEventListener('click', ligarLampada);
-botaoDesligar.addEventListener('click', desligarLampada);
 estadoDaLampada.addEventListener('mouseover', ligarLampada);
 
+botaoLigarDesligar.addEventListener('click', () => {
+    if(botaoLigarDesligar.classList[0] == 'ligar') {
+        ligarLampada();
+        botaoLigarDesligar.classList.remove('ligar');
+        botaoLigarDesligar.classList.add('desligar');
+    } else if(botaoLigarDesligar.classList[0] == 'desligar') {
+        desligarLampada();
+        botaoLigarDesligar.classList.remove('desligar');
+        botaoLigarDesligar.classList.add('ligar');
+    }
+});
+
 function estaQuebrada() {
-    return estadoDaLampada.src.indexOf('quebrada') > -1
+    return estadoDaLampada.src.indexOf('quebrada') > -1;
 }
 
 function ligarLampada() {
